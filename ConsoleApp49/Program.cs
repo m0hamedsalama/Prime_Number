46cf4df11a27f9750af6c533cd865c978593b20a
m0hamedsalama <121294189+m0hamedsalama@users.noreply.github.com>
2022-12-23 14:11:27 +0200

    Add files via upload

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                    continue;
+                }
+            }
+
+        }
+    }
+}
